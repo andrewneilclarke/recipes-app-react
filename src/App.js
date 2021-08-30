11470a9978620f790c1a9ac959cd0034bbb46dcc
andrewneilclarke <andrewneilclarke@gmail.com>
2021-08-30 17:37:56 +0200

    added design

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 const App = () => {
 
   const API_ID = '332736e6';
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  // const API_KEY = process.env.REACT_APP_API_KEY;
   const [recipes, setRecipes] = useState([]) ;
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('')
 
   useEffect(() => {
       const getRecipes = async () => {
-      const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`);
+      const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${process.env.REACT_APP_API_KEY}`);
       const data = await res.json();
       setRecipes(data.hits);
    }
